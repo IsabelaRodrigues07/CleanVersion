29f3f720ecbe3d65f8c82d3b48e094626432c5b2
Isabela <isarodrigues0703@outlook.com>
2022-09-20 03:27:13 -0300

    Add project files.

---
+++ dbo.Produto.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Produto] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Nome] NVARCHAR (MAX) NOT NULL,
+	[Quantidade] INT  NOT NULL,
+	[Categoria] NVARCHAR (MAX) NOT NULL,
+    [Preco] INT NOT NULL, 
+    CONSTRAINT [PK_Produto] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
